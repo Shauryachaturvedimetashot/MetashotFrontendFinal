1070ee95ce93099dfd6d41b141a12ef918e33c2d
Shauryachaturvedimetashot <shaurya@metashot.co.in>
2024-06-27 12:49:53 +0530

    Update page.tsx

--- src/app/addQuestions/page.tsx
+++ src/app/addQuestions/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 import axios from "axios";
+import Navbarn from "../../Components/Navbarn";
+import Sidebarn from "../../Components/SidebarN";
 
 interface Category {
   name: string;
@@ -16,13 +17,11 @@ const AskQuestions = () => {
   const [newCategory, setNewCategory] = useState<string>("");
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState<boolean>(false);
   const [categoryToDelete, setCategoryToDelete] = useState<string>("");
-  const [initialSkills, setInitialSkills] = useState<string[]>([]);
 
   useEffect(() => {
     const storedSkillsStr = localStorage.getItem("skills");
     if (storedSkillsStr) {
       const storedSkills = JSON.parse(storedSkillsStr);
-      setInitialSkills(storedSkills);
       setCategories(storedSkills.map((skill: string) => ({ name: skill, questions: [] })));
     }
   }, []);
@@ -49,29 +48,25 @@ const AskQuestions = () => {
     }
   };
 
-  const handleQuestionDelete = (index: number) => {
-    if (selectedCategory !== null) {
-      const updatedCategories = [...categories];
-      const categoryIndex = updatedCategories.findIndex((cat) => cat.name === selectedCategory);
-      if (categoryIndex !== -1) {
-        updatedCategories[categoryIndex].questions.splice(index, 1);
-        setCategories(updatedCategories);
-      }
+  const handleQuestionDelete = (categoryName: string, index: number) => {
+    const updatedCategories = [...categories];
+    const categoryIndex = updatedCategories.findIndex((cat) => cat.name === categoryName);
+    if (categoryIndex !== -1) {
+      updatedCategories[categoryIndex].questions.splice(index, 1);
+      setCategories(updatedCategories);
     }
   };
 
-  const handleEditClick = (index: number) => {
-    if (selectedCategory !== null) {
-      const categoryIndex = categories.findIndex((cat) => cat.name === selectedCategory);
-      if (categoryIndex !== -1) {
-        setNewQuestion(categories[categoryIndex].questions[index]);
-        setEditIndex(index);
-      }
+  const handleEditClick = (categoryName: string, index: number) => {
+    const categoryIndex = categories.findIndex((cat) => cat.name === categoryName);
+    if (categoryIndex !== -1) {
+      setNewQuestion(categories[categoryIndex].questions[index]);
+      setEditIndex(index);
     }
   };
 
   const handleCategorySelection = (categoryName: string) => {
-    setSelectedCategory(categoryName);
+    setSelectedCategory(categoryName === "__all__" ? null : categoryName);
   };
 
   const handleAddCategory = () => {
@@ -126,7 +121,7 @@ const AskQuestions = () => {
   const handleFinishButtonClick = async () => {
     try {
       const formData = JSON.parse(localStorage.getItem("formData") || "{}");
-      const technicalSkills = formData.technicalSkills || initialSkills;
+      const technicalSkills = formData.technicalSkills || categories.map(cat => cat.name);
 
       const payload = {
         jobPosition: formData.jobPosition || "",
@@ -153,118 +148,138 @@ const AskQuestions = () => {
 
       if (response.status === 200) {
         console.log("Interview created successfully:", response.data);
-        window.location.href = "/PastInterviews";
+        window.location.href = "/Invite";
       } else {
         console.error("Failed to create interview:", response.data);
       }
     } catch (error) {
       console.error("Error creating interview:", error);
+      alert("Error creating interview. Please fill out the details correctly.");
     }
   };
 
-  // Remove duplicates from initialSkills and categories
-  const allSkills = Array.from(new Set([...initialSkills, ...categories.map(cat => cat.name)]));
+  // Render questions based on selectedCategory or all categories
+  const renderedQuestions = selectedCategory !== null
+    ? categories.find(cat => cat.name === selectedCategory)?.questions || []
+    : categories.reduce((acc, cat) => [...acc, ...cat.questions], []);
 
   return (
-    <div className="flex flex-col items-center justify-center bg-green-100 min-h-screen relative">
-      <div className="absolute top-4 left-4 text-black">
-        <Link href="/InterviewForm">← Back</Link>
-      </div>
-      <h1 className="text-3xl font-semibold mb-4 text-center text-black">Questions</h1>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4" onClick={handleGenerateQuestions}>
-        Generate Using AI
-      </button>
-      <div className="w-full max-w-md">
-        <h2 className="text-lg font-semibold mb-2 text-black">Add Specific Questions</h2>
-        <textarea
-          className="border border-gray-300 rounded px-3 py-2 mb-2 w-full focus:outline-none focus:border-blue-400 text-black"
-          value={newQuestion}
-          onChange={(e) => setNewQuestion(e.target.value)}
-          placeholder="Type your question here..."
-        />
-        <div className="mb-2">
-          <label className="text-sm font-semibold text-black">Select or Add Skill:</label>
-          <select
-            className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 text-black"
-            value={selectedCategory || ""}
-            onChange={(e) => handleCategorySelection(e.target.value)}
-          >
-            <option value="">Select Skill or Add New</option>
-            {allSkills.map((skill, index) => (
-              <option key={index} value={skill}>{skill}</option>
-            ))}
-          </select>
-          <input
-            type="text"
-            className="border border-gray-300 rounded px-3 py-2 mt-2 w-full focus:outline-none focus:border-blue-400 text-black"
-            value={newCategory}
-            onChange={(e) => setNewCategory(e.target.value)}
-            placeholder="New Skill Name"
-          />
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-2"
-            onClick={handleAddCategory}
-          >
-            Add Skill
-          </button>
-        </div>
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4"
-          onClick={addOrUpdateQuestion}
-        >
-          {editIndex !== null ? "Update Question" : "Add Question"}
-        </button>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold mb-2 text-black">Added Questions for {selectedCategory}</h2>
-          <ul>
-            {categories
-              .find((cat) => cat.name === selectedCategory)
-              ?.questions.map((question, index) => (
-                <li key={index} className="border border-gray-300 rounded px-3 py-2 mb-2 text-black">
-                  {question}
-                  <button
-                    className="ml-2 text-sm text-red-600"
-                    onClick={() => handleQuestionDelete(index)}
+    <div className="bg-green-100 min-h-screen">
+      <Navbarn />
+      <div className="flex">
+        <Sidebarn />
+        <div className="flex-1 p-4">
+          <div className="mb-4">
+            <div className="flex">
+              <div className="w-1/2 pr-4">
+                <h2 className="text-lg font-semibold mb-2 text-black">Add Specific Questions</h2>
+                <textarea
+                  className="border border-gray-300 rounded px-3 py-2 mb-2 w-full focus:outline-none focus:border-blue-400 text-black"
+                  value={newQuestion}
+                  onChange={(e) => setNewQuestion(e.target.value)}
+                  placeholder="Type your question here..."
+                />
+                <div className="mb-2">
+                  <label className="text-sm font-semibold text-black">Select or Add Skill:</label>
+                  <select
+                    className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 text-black"
+                    value={selectedCategory || "__all__"}
+                    onChange={(e) => handleCategorySelection(e.target.value)}
                   >
-                    Delete
-                  </button>
-                  <button
-                    className="ml-2 text-sm text-blue-600"
-                    onClick={() => handleEditClick(index)}
-                  >
-                    Edit
-                  </button>
-                </li>
-              ))}
-          </ul>
-        </div>
-        {showDeleteConfirmation && (
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-4 rounded shadow-lg text-center">
-              <p className="mb-4">Are you sure you want to delete {categoryToDelete}?</p>
-              <div>
-                <button
-                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mr-2"
-                  onClick={handleConfirmDelete}
-                >
-                  Yes
-                </button>
-                <button
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-2"
-                  onClick={handleCancelDelete}
-                >
-                  No
-                </button>
+                    <option value="__all__">Show All</option>
+                    {categories.map((cat, index) => (
+                      <option key={index} value={cat.name}>{cat.name}</option>
+                    ))}
+                  </select>
+                  <input
+                    type="text"
+                    className="border border-gray-300 rounded px-3 py-2 mt-2 w-full focus:outline-none focus:border-blue-400 text-black"
+                    value={newCategory}
+                    onChange={(e) => setNewCategory(e.target.value)}
+                    placeholder="New Skill Name"
+                  />
+                </div>
+              </div>
+              <div className="w-1/2 pl-4">
+                <div className="mb-4">
+                  <h2 className="text-lg font-semibold mb-2 text-black">Added Questions</h2>
+                  <ul>
+                    {renderedQuestions.map((question, index) => (
+                      <li key={index} className="border border-gray-300 rounded px-3 py-2 mb-2 text-black">
+                        {question}
+                        {selectedCategory !== null && (
+                          <>
+                            <button
+                              className="ml-2 text-sm text-red-600"
+                              onClick={() => handleQuestionDelete(selectedCategory!, index)}
+                            >
+                              Delete
+                            </button>
+                            <button
+                              className="ml-2 text-sm text-blue-600"
+                              onClick={() => handleEditClick(selectedCategory!, index)}
+                            >
+                              Edit
+                            </button>
+                          </>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {showDeleteConfirmation && (
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-4 rounded shadow-lg text-center">
+                      <p className="mb-4">Are you sure you want to delete {categoryToDelete}?</p>
+                      <div>
+                        <button
+                          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mr-2"
+                          onClick={handleConfirmDelete}
+                        >
+                          Yes
+                        </button>
+                        <button
+                          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-2"
+                          onClick={handleCancelDelete}
+                        >
+                          No
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
+            <div className="mt-4 flex items-center">
+              <button
+                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-500 mr-2"
+                onClick={addOrUpdateQuestion}
+              >
+                {editIndex !== null ? "Update Question" : "Add Question"}
+              </button>
+              <button
+                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-500 mr-2"
+                onClick={handleGenerateQuestions}
+              >
+                Generate Using AI
+              </button>
+              <button
+                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-500"
+                onClick={handleAddCategory}
+              >
+                Add Skill
+              </button>
+            </div>
+            <div className="mt-4">
+              <button
+                className="bg-green-600 text-white px-4 py-2 mt-8 rounded hover:bg-green-600 text-center"
+                onClick={handleFinishButtonClick}
+              >
+                Finish
+              </button>
+            </div>
           </div>
-        )}
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mt-4"
-          onClick={handleFinishButtonClick}
-        >
-          Finish
-        </button>
+        </div>
       </div>
     </div>
   );
