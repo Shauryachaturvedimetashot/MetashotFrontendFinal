b9614cbaafbc07982fceabda9d86c97d882c8471
Shauryachaturvedimetashot <shaurya@metashot.co.in>
2024-06-28 13:12:05 +0530

    Update SidebarN.tsx

--- src/Components/SidebarN.tsx
+++ src/Components/SidebarN.tsx
@@ -1,56 +1,50 @@
-import React, { useEffect } from 'react'
+import React from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse,faInbox,faCalendar,faHandshake,faGear,faBolt} from "@fortawesome/free-solid-svg-icons";
-const Sidebarn:React.FC=()=> {
-  useEffect(()=>{
-    const handleFontAwesomeLoaded = ()=>{
-      document.querySelectorAll('.icon-hidden').forEach((el)=>{
-        el.classList.remove('icon-hidden')
-      })
+import { faHouse, faInbox, faEnvelope, faCalendar, faHandshake, faGear } from "@fortawesome/free-solid-svg-icons";
 
-    }
-    const fastyles = document.querySelector('link[href*="fontawesome"]')
-    if(fastyles){
-      fastyles.addEventListener('load',handleFontAwesomeLoaded)
-    }
-    else{
-      handleFontAwesomeLoaded()
-    }
-  },[])
-
-
-
-
+const Sidebarn: React.FC = () => {
   return (
-   <>
-   <div className='w-20p'>
-   <div className='flex flex-col '>
-   <div className='flex flex-col py-10 items-start sidebarN '>
-   <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
-   <Link href="/dashboard" className='href'/>
-   <FontAwesomeIcon icon={faHouse} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Dashboard</button>
-    <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
-    <Link href="/dashboard" className='href'/>
-    <FontAwesomeIcon icon={faInbox} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Hiring</button>
-    <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
-    <Link href="/dashboard"  className='href'/>
-    <FontAwesomeIcon icon={faHandshake} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Jobs</button>
-    <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
-    <Link href="/dashboard" className='href'/>
-    <FontAwesomeIcon icon={faBolt} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Messages</button>
-    <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
-    <Link href="/dashboard" className='href'/>
-    <FontAwesomeIcon icon={faCalendar} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Calendar</button>
+    <div className='w-1/4'>
+      <div className='flex flex-col'>
+        <div className='flex flex-col py-10 items-start sidebarN'>
+          <Link href="/Jobs">
+            <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
+              <FontAwesomeIcon icon={faHouse} className="fas fa-check mr-5" style={{ color: "black" }} />Dashboard
+            </button>
+          </Link>
+          <Link href="/InterviewForm">
+            <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
+              <FontAwesomeIcon icon={faInbox} className="fas fa-check mr-5" style={{ color: "black" }} />Hiring
+            </button>
+          </Link>
+          <Link href="/PastInterviews">
+            <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
+              <FontAwesomeIcon icon={faHandshake} className="fas fa-check mr-5" style={{ color: "black" }} />Jobs
+            </button>
+          </Link>
+          <Link href="/messages">
+            <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
+              <FontAwesomeIcon icon={faEnvelope} className="fas fa-check mr-5" style={{ color: "black" }} />Messages
+            </button>
+          </Link>
+          <Link href="/calendar">
+            <button className='py-4 pl-12 btnN rounded-r-[16px] mb-2 hover:text-lg'>
+              <FontAwesomeIcon icon={faCalendar} className="fas fa-check mr-5" style={{ color: "black" }} />Calendar
+            </button>
+          </Link>
+        </div>
+        <div className='py-40 px-7 sidebarN cursor-pointer'>
+          <Link href="/Settings">
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faGear} className="fas fa-check mr-5" style={{ color: "black" }} />
+              <span className="hover:text-lg">Settings</span>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
-    <div className='py-40 px-7 sidebarN cursor-pointer'>
-        <Link href="/dashboard" className='href' />
-    <FontAwesomeIcon icon={faGear} className="fas fa-check mr-5 icon-hidden" style={{ color: "black" }} />Settings</div>
-
-   </div>
-   </div>
-   </>
-  )
+  );
 }
 
-export default Sidebarn
+export default Sidebarn;
