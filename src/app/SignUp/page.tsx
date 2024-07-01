da81a743280e79243addefe4037ddb45dd08b7fb
Shauryachaturvedimetashot <shaurya@metashot.co.in>
2024-07-01 18:04:23 +0530

    Update page.tsx

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from "react";
 import Link from "next/link";
-iimport { useRouter } from "next/navigation"; // Changed from 'next/navigation' to 'next/router'
+import { useRouter } from "next/navigation"; // Changed from 'next/navigation' to 'next/router'
 import apiClient from "../../utils/axiosSetup"; // Assuming axios setup is correctly imported
 
 const LoginSignUpPage = () => {
