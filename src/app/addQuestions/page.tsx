90e3f60f6d14dc612c3d5c6fc56262d9622c6488
Shauryachaturvedimetashot <shaurya@metashot.co.in>
2024-07-02 16:40:05 +0530

    Update page.tsx

--- src/app/addQuestions/page.tsx
+++ src/app/addQuestions/page.tsx
@@ -276,7 +276,7 @@ const AskQuestions = () => {
       {showDeleteConfirmation && (
         <div className="fixed top-0 left-0 right-0 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
           <div className="p-6 bg-white rounded shadow-md">
-            <p>Are you sure you want to delete the skill "{categoryToDelete}"?</p>
+<p>Are you sure you want to delete the skill &quot;{categoryToDelete}&quot;?</p>
             <div className="mt-4">
               <button
                 className="px-4 py-2 mr-2 text-white bg-red-500 rounded hover:bg-red-600"
