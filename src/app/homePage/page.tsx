ba72d0701dbeca22d7ad77c6398b757a7b83f0f4
Raees Areekan <raees@metashot.co.in>
2024-07-06 01:07:31 +0530

    started the homepage

---
+++ src/app/homePage/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Content1 from "./Content1";
+
+const page:React.FC = ()=>{
+
+    return(
+        <div>
+            <Navbar/>
+            <Content1/>
+        </div>
+    )
+
+}
+
+export default page
