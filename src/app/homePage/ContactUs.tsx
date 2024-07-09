f4fc3a794c70ca616e333a4c8aef9fae50176d6c
Raees-Metashot <raees@metashot.co.in>
2024-07-09 14:50:12 +0530

    finished adding contactUs component

--- src/app/homePage/ContactUs.tsx
+++ src/app/homePage/ContactUs.tsx
@@ -10,7 +10,7 @@ const ContactUs:React.FC = ()=>{
 
             </h1>
             <div className="lg:w-[25%] xl:w-[20%] ml-[2%]">
-            Connect with us: Let&apos;s Discuss Your Digital Marketing Needs
+            Connect with us: Let&apos;s Discuss Your Interview Needs
 
             </div>
 
@@ -19,20 +19,42 @@ const ContactUs:React.FC = ()=>{
         <div className="flex flex-col lg:flex-row items-center justify-center mt-1p mb:mt-3p">
 
             
-        <div className="w-[80%] h-[65vh] bg-[#EAFBEE] rounded-xl text-black">
-        <div className="flex flex-row justify-between">
+        <div className="w-[80%] h-[75%] bg-[#EAFBEE] rounded-xl text-black">
+        <div className="flex flex-row justify-between ml-[7%]">
 
             
-        <div className="flex mb-4 items-center justify-center">
-           <label className="flex items-center mr-4">
+        <div className="flex flex-col mb-4 text-lg mt-[5%] w-1/2">
+            <div>
+            <label className=" mr-4">
             <input type="radio" name="contact" className="mr-2" /> Say Hi
            </label>
-           <label className="flex items-center">
+           <label className="">
             <input type="radio" name="contact" className="mr-2" /> Get a Quote
            </label>
+                
+            </div>
+           
+           <div>
+           <form>
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2 text-sm text-gray-700">Name</label>
+            <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded-md" placeholder="Name" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2 text-sm text-gray-700">Email*</label>
+            <input type="email" id="email" className="w-full p-2 border border-gray-300 rounded-md" placeholder="Email" required />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block mb-2 text-sm text-gray-700">Message*</label>
+            <textarea id="message" className="w-full p-2 border border-gray-300 rounded-md" placeholder="Message" rows={4} required></textarea>
+          </div>
+          <button type="submit" className="w-full bg-green-700 text-white p-2 rounded-md">Send Message</button>
+        </form>
+           </div>
          </div>
+        
 
-         <div className="flex items-center justify-center">
+         <div className="flex items-center ">
          <Image src="/Illustration3.png" alt="Illustration3" width={200} height={191} className="object-contain" />
 
          </div>
