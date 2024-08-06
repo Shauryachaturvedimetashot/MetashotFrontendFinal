1cb124d53405f6cc5ec3c702a652d50aa08b853b
Sony Thomas <goodtobetrue2@gmail.com>
2024-08-06 14:37:30 +0530

    fix apostrophe

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -36,7 +36,7 @@ const ForgotPassword = () => {
                         Forgot Your Password?
                     </h2>
                     <p className="mt-2 text-center text-sm text-gray-600">
-                        Enter your email address and we'll send you a link to reset your password.
+                        Enter your email address and we&apos;ll send you a link to reset your password.
                     </p>
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
