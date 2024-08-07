744ecfbff91316f48534757c698c6a1912e1acd7
Sony Thomas <goodtobetrue2@gmail.com>
2024-08-07 21:00:09 +0530

    add export candidate logic

--- src/app/PastInterviews/page.tsx
+++ src/app/PastInterviews/page.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import NavbarN from "../../Components/Navbarn";
 import SidebarN from "../../Components/SidebarN";
 import styles from "./PastInterviews.module.css";
-
+import { IoMdCloudDownload } from "react-icons/io";
 interface Interview {
   _id: string;
   jobPosition: string;
@@ -20,32 +20,48 @@ interface Interview {
 
 const Interviews: React.FC = () => {
   const [interviews, setInterviews] = useState<Interview[]>([]);
-  const [selectedInterview, setSelectedInterview] = useState<Interview | null>(null);
+  const [selectedInterview, setSelectedInterview] = useState<Interview | null>(
+    null
+  );
 
   const fetchInterviews = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.get("https://metashot-backend.azurewebsites.net/interview/getAll", {
-        headers: { Authorization: `Bearer ${token}` }
-      });
+      const response = await axios.get(
+        "https://metashot-backend.azurewebsites.net/interview/getAll",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       const interviews = response.data;
 
-      const enrichedInterviews = await Promise.all(interviews.map(async (interview: any) => {
-        const scheduleResponse = await axios.get(`https://metashot-backend.azurewebsites.net/interview/schedule?interview=${interview._id}`, {
-          headers: { Authorization: `Bearer ${token}` }
-        });
-        const scheduleData = scheduleResponse.data;
-        const recentSchedule = scheduleData.sort((a: any, b: any) => new Date(b.end).getTime() - new Date(a.end).getTime())[0];
-        const toTime = recentSchedule ? recentSchedule.end : interview.updatedAt;
+      const enrichedInterviews = await Promise.all(
+        interviews.map(async (interview: any) => {
+          const scheduleResponse = await axios.get(
+            `https://metashot-backend.azurewebsites.net/interview/schedule?interview=${interview._id}`,
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            }
+          );
+          const scheduleData = scheduleResponse.data;
+          const recentSchedule = scheduleData.sort(
+            (a: any, b: any) =>
+              new Date(b.end).getTime() - new Date(a.end).getTime()
+          )[0];
+          const toTime = recentSchedule
+            ? recentSchedule.end
+            : interview.updatedAt;
 
-        return {
-          ...interview,
-          from: new Date(interview.createdAt).toLocaleDateString(),
-          to: new Date(toTime).toLocaleDateString()
-        };
-      }));
+          return {
+            ...interview,
+            from: new Date(interview.createdAt).toLocaleDateString(),
+            to: new Date(toTime).toLocaleDateString(),
+          };
+        })
+      );
 
       setInterviews(enrichedInterviews);
+      // console.log("Interviews fetched:", enrichedInterviews);
     } catch (error) {
       console.error("Error fetching interviews:", error);
     }
@@ -60,19 +76,25 @@ const Interviews: React.FC = () => {
   };
 
   const handleCloseModal = (e: React.MouseEvent<HTMLDivElement>) => {
-    if ((e.target as HTMLElement).className.includes('detailsModal')) {
+    if ((e.target as HTMLElement).className.includes("detailsModal")) {
       setSelectedInterview(null);
     }
   };
 
-  const handleStatusToggle = async (interviewId: string, currentStatus: string) => {
+  const handleStatusToggle = async (
+    interviewId: string,
+    currentStatus: string
+  ) => {
     try {
       const token = localStorage.getItem("token");
       await axios.put(
         `https://metashot-backend.azurewebsites.net/interview/changeStatus`,
-        { interviewId, status: currentStatus === "active" ? "deactive" : "active" },
         {
-          headers: { Authorization: `Bearer ${token}` }
+          interviewId,
+          status: currentStatus === "active" ? "deactive" : "active",
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
         }
       );
       await fetchInterviews(); // Refresh the interviews list after status change
@@ -90,7 +112,51 @@ const Interviews: React.FC = () => {
       alert("You need to activate the interview to add more candidates.");
     }
   };
-
+  const handleSubmit = async (interviewId: string) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get(
+        `http://localhost:8000/interview/download?interviewId=${interviewId}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          responseType: 'blob'
+        }
+      );
+      
+      // Check if the response is actually an Excel file
+      if (response.headers['content-type'] === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+        // Process the Excel file as before
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `candidates_${interviewId}.xlsx`);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      }  else {
+        // It's not an Excel file, so it's probably a JSON error
+        const reader = new FileReader();
+        reader.onload = function() {
+          const errorMessage = JSON.parse(reader.result as string);
+          console.error("Server returned an error:", errorMessage.error);
+          // Handle the error appropriately
+        };
+        reader.readAsText(response.data);
+      }
+    } catch (error:any) {
+      if (error.response && error.response.data) {
+        const reader = new FileReader();
+        reader.onload = function() {
+          const errorMessage = JSON.parse(reader.result as string);
+          console.error("Error downloading interviews:", errorMessage.error);
+          // Handle the error appropriately
+        };
+        reader.readAsText(error.response.data);
+      } else {
+        console.error("Error downloading interviews:", error.message);
+      }
+    }
+  };
   return (
     <div className={styles.container}>
       <NavbarN />
@@ -107,31 +173,57 @@ const Interviews: React.FC = () => {
             {interviews.length === 0 ? (
               <p>No interviews found.</p>
             ) : (
-              interviews.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()).map((interview) => (
-                <div key={interview._id} className={styles.interviewCard}>
-                  <h2 className={styles.jobPosition}>{interview.jobPosition} - {interview.yearsOfExperience} years experience</h2>
-                  <p className={styles.jobDescription}>{interview.jobDescription}</p>
-                  <p className={styles.status}>Status: {interview.status}</p>
-                  <button
-                    className={interview.status === "active" ? styles.deactivateButton : styles.activateButton}
-                    onClick={() => handleStatusToggle(interview._id, interview.status)}
-                  >
-                    {interview.status === "active" ? "Deactivate it" : "Activate it"}
-                  </button>
-                  <button
-                    className={styles.detailsButton}
-                    onClick={() => handleShowDetails(interview)}
-                  >
-                    Show Details
-                  </button>
-                  <button
-                    className={styles.addCandidatesButton}
-                    onClick={() => handleAddCandidates(interview._id, interview.status)}
-                  >
-                    Add New Candidates
-                  </button>
-                </div>
-              ))
+              interviews
+                .sort(
+                  (a, b) =>
+                    new Date(b.createdAt).getTime() -
+                    new Date(a.createdAt).getTime()
+                )
+                .map((interview) => (
+                  <div key={interview._id} className={styles.interviewCard}>
+                    <div className="flex justify-between">
+                      <h2 className={styles.jobPosition}>
+                        {interview.jobPosition} - {interview.yearsOfExperience}{" "}
+                        years experience
+                      </h2>
+                      <button  onClick={()=>handleSubmit(interview._id)} className="bg-green-500 hover:bg-green-700/70 rounded-md p-2">
+                        <IoMdCloudDownload/>
+                      </button>
+                    </div>
+                    <p className={styles.jobDescription}>
+                      {interview.jobDescription}
+                    </p>
+                    <p className={styles.status}>Status: {interview.status}</p>
+                    <button
+                      className={
+                        interview.status === "active"
+                          ? styles.deactivateButton
+                          : styles.activateButton
+                      }
+                      onClick={() =>
+                        handleStatusToggle(interview._id, interview.status)
+                      }
+                    >
+                      {interview.status === "active"
+                        ? "Deactivate it"
+                        : "Activate it"}
+                    </button>
+                    <button
+                      className={styles.detailsButton}
+                      onClick={() => handleShowDetails(interview)}
+                    >
+                      Show Details
+                    </button>
+                    <button
+                      className={styles.addCandidatesButton}
+                      onClick={() =>
+                        handleAddCandidates(interview._id, interview.status)
+                      }
+                    >
+                      Add New Candidates
+                    </button>
+                  </div>
+                ))
             )}
           </div>
         </div>
@@ -140,18 +232,42 @@ const Interviews: React.FC = () => {
         <div className={styles.detailsModal} onClick={handleCloseModal}>
           <div className={styles.detailsContent}>
             <h2>{selectedInterview.jobPosition}</h2>
-            <p><strong>Job Description:</strong> {selectedInterview.jobDescription}</p>
-            <p><strong>Experience Required:</strong> {selectedInterview.yearsOfExperience} years</p>
-            <p><strong>Skills Required:</strong> {selectedInterview.technicalSkills.join(", ")}</p>
-            <p><strong>Date Created:</strong> {new Date(selectedInterview.createdAt).toLocaleString()}</p>
-            <p><strong>From:</strong> {selectedInterview.from}</p>
-            <p><strong>To:</strong> {selectedInterview.to}</p>
+            <p>
+              <strong>Job Description:</strong>{" "}
+              {selectedInterview.jobDescription}
+            </p>
+            <p>
+              <strong>Experience Required:</strong>{" "}
+              {selectedInterview.yearsOfExperience} years
+            </p>
+            <p>
+              <strong>Skills Required:</strong>{" "}
+              {selectedInterview.technicalSkills.join(", ")}
+            </p>
+            <p>
+              <strong>Date Created:</strong>{" "}
+              {new Date(selectedInterview.createdAt).toLocaleString()}
+            </p>
+            <p>
+              <strong>From:</strong> {selectedInterview.from}
+            </p>
+            <p>
+              <strong>To:</strong> {selectedInterview.to}
+            </p>
             <div className={styles.buttonGroup}>
-              <Link href={`/Invite?interview=${selectedInterview._id}`} legacyBehavior>
+              <Link
+                href={`/Invite?interview=${selectedInterview._id}`}
+                legacyBehavior
+              >
                 <a className={styles.addCandidatesButton}>Add New Candidates</a>
               </Link>
-              <Link href={`/Candidates?interview=${selectedInterview._id}`} legacyBehavior>
-                <a className={styles.scheduledInterviewsButton}>View Candidates</a>
+              <Link
+                href={`/Candidates?interview=${selectedInterview._id}`}
+                legacyBehavior
+              >
+                <a className={styles.scheduledInterviewsButton}>
+                  View Candidates
+                </a>
               </Link>
             </div>
           </div>
